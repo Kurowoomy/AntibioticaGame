d5511264dae3c05667599956381740cfb6dadd29
Emma Eliasson <gurgelsnurp@hotmail.se>
2020-11-26 15:34:21 +0100

    Info buttons added. Class done.

--- www/js/infoWindow.js
+++ www/js/infoWindow.js
@@ -1,16 +1,58 @@
-document.getElementById("infoText").innerHTML = text;
-var modal = document.getElementById("modal");
-var btn = document.getElementById("infoButton").onclick = function() {
-    modal.style.visibility = "hidden";
-}
+class InfoWindow { // För att skapa objekt som kan redigeras
+    constructor() {
+        // Variabler redigerbara av knappen
+        this.text = document.createElement("div");
+        this.image = document.createElement("img");
+        
+        // Yta som täcker allt på skärmen och innehåller fönstret
+        this.modal = document.createElement("div");
+        this.modal.id = "infoModal";
+        this.modal.classList.add("infoModal"); // css
+        
+        // Fönstret
+        this.window = document.createElement("div");
+        this.window.id = "window";
+        this.window.classList.add("infoWindow");
 
-class InfoWindow {
-    constructor(infoText) {
-        this.text = infoText;
+        // Stäng-knapp
+        this.button = document.createElement("button");
+        this.button.innerHTML = "Got it!";
+        var obj = this; // kan ej nå this i funktionen nedan
+        this.button.onclick = function() {
+            obj.modal.style.visibility = "hidden";
+        };
     }
+}
+// Metoder för InfoWindow. Prototype gör så att det går att använda this
+InfoWindow.prototype.setAndShowContent = function(infoText, imageSrc) {
+    this.text.innerHTML = infoText;
+    this.image.src = imageSrc;
 
+    this.modal.style.visibility = "visible";
 }
 
-function showInfoWindow(infoWindowObject) {
 
+
+// Global variabel som alla infoknappar kan ändra innehåll på
+var infoWindow = new InfoWindow();
+
+// Skapar info-objekten som syns i html
+function createInfoWindow(visibility) {
+    infoWindow.modal.style.visibility = visibility;
+    document.body.appendChild(infoWindow.modal);
+    document.getElementById("infoModal").appendChild(infoWindow.window);
+    document.getElementById("window").appendChild(infoWindow.text);
+    document.getElementById("window").appendChild(infoWindow.button);
+    document.getElementById("window").appendChild(infoWindow.image);
+}
+
+// Skapar en knapp som ändrar och visar upp inforutan
+function createInfoButton(infoText, imageSrc) {
+    var infoButton = document.createElement("button"); // objekt
+    infoButton.classList.add("exampleButton"); // css
+    document.body.appendChild(infoButton); // html-objekt
+
+    infoButton.onclick = function() {
+        infoWindow.setAndShowContent(infoText, imageSrc);
+    }
 }
